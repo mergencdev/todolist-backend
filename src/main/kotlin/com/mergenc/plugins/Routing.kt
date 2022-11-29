32829e0030a587fd9a9a89451f645fd1ea8039ec
Mehmet Emin Ergenc <mergencdev@gmail.com>
2022-11-29 15:11:36 +0300

    -Add CRUD endpoints

--- src/main/kotlin/com/mergenc/plugins/Routing.kt
+++ src/main/kotlin/com/mergenc/plugins/Routing.kt
@@ -1,9 +1,12 @@
 package com.mergenc.plugins
 
 import com.mergenc.entities.ToDo
+import com.mergenc.entities.ToDoDraft
 import com.mergenc.repository.InMemoryToDoRepository
 import com.mergenc.repository.ToDoRepository
+import io.ktor.http.*
 import io.ktor.server.application.*
+import io.ktor.server.request.*
 import io.ktor.server.response.*
 import io.ktor.server.routing.*
 
@@ -37,15 +40,50 @@ fun Application.configureRouting() {
         }
 
         post("/todos") {
-
+            val todoDraft = call.receive<ToDoDraft>()
+            val todo = repository.addTodo(todoDraft)
+            call.respond(todo)
         }
 
         put("/todos/{id}") {
+            val todoDraft = call.receive<ToDoDraft>()
+            val todoId = call.parameters["id"]?.toIntOrNull()
 
+            if (todoId == null) {
+                call.respond(
+                    HttpStatusCode.BadRequest, "id parameter has to be a number!"
+                )
+                return@put
+            }
+
+            val updated = repository.updateTodo(todoId, todoDraft)
+            if (updated) {
+                call.respond(HttpStatusCode.OK)
+            } else {
+                call.respond(
+                    HttpStatusCode.NotFound, "found no todo with the id $todoId"
+                )
+            }
         }
 
         delete("/todos/{id}") {
+            val todoId = call.parameters["id"]?.toIntOrNull()
 
+            if (todoId == null) {
+                call.respond(
+                    HttpStatusCode.BadRequest, "id parameter has to be a number!"
+                )
+                return@delete
+            }
+
+            val removed = repository.removeTodo(todoId)
+            if (removed) {
+                call.respond(HttpStatusCode.OK)
+            } else {
+                call.respond(
+                    HttpStatusCode.NotFound, "found no todo with the id $todoId"
+                )
+            }
         }
     }
     routing {}
