1df4ac85e2184277f2f773007fe2255fea3865bb
Mehmet Emin Ergenc <mergencdev@gmail.com>
2022-11-29 15:10:29 +0300

    -Add operations

--- src/main/kotlin/com/mergenc/repository/ToDoRepository.kt
+++ src/main/kotlin/com/mergenc/repository/ToDoRepository.kt
@@ -1,8 +1,16 @@
 package com.mergenc.repository
 
 import com.mergenc.entities.ToDo
+import com.mergenc.entities.ToDoDraft
 
 interface ToDoRepository {
     fun getAllToDos(): List<ToDo>
+
     fun getSingleToDo(id: Int): ToDo?
+
+    fun addTodo(draft: ToDoDraft): ToDo
+
+    fun removeTodo(id: Int): Boolean
+
+    fun updateTodo(id: Int, draft: ToDoDraft): Boolean
 }
