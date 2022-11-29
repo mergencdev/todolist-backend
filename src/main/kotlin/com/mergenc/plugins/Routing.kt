b2e06ee88fea4398e450692b885b60837c5511da
Mehmet Emin Ergenc <mergencdev@gmail.com>
2022-11-29 14:52:59 +0300

    -Get dummies from repository

--- src/main/kotlin/com/mergenc/plugins/Routing.kt
+++ src/main/kotlin/com/mergenc/plugins/Routing.kt
@@ -1,6 +1,8 @@
 package com.mergenc.plugins
 
 import com.mergenc.entities.ToDo
+import com.mergenc.repository.InMemoryToDoRepository
+import com.mergenc.repository.ToDoRepository
 import io.ktor.server.application.*
 import io.ktor.server.response.*
 import io.ktor.server.routing.*
@@ -8,23 +10,30 @@ import io.ktor.server.routing.*
 fun Application.configureRouting() {
     // Starting point for a Ktor app:
     routing {
-        val todos = listOf<ToDo>(
-            ToDo(1, "Homework", true),
-            ToDo(2, "Internet Programlama", false),
-            ToDo(3, "Backend", true),
-        )
+        val repository: ToDoRepository = InMemoryToDoRepository()
 
         get("/") {
             call.respondText("Hello World!")
         }
 
         get("/todos") {
-            call.respond(todos)
+            call.respond(repository.getAllToDos())
         }
 
         get("/todos/{id}") {
-            val id = call.parameters["id"]
-            call.respondText("Todolist details: #$id")
+            val id = call.parameters["id"]?.toIntOrNull()
+            if (id == null) {
+                call.respondText("Invalid or missing id", status = io.ktor.http.HttpStatusCode.BadRequest)
+                return@get
+            }
+
+            var todo = repository.getSingleToDo(id)
+            if (todo == null) {
+                call.respondText("No todo with id $id", status = io.ktor.http.HttpStatusCode.NotFound)
+                return@get
+            } else {
+                call.respond(todo)
+            }
         }
 
         post("/todos") {
