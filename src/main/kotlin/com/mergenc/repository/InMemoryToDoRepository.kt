9082e8fb9259f39d0cb9396f7907af654c23c400
Mehmet Emin Ergenc <mergencdev@gmail.com>
2022-11-29 15:11:14 +0300

    -CRUD operations

--- src/main/kotlin/com/mergenc/repository/InMemoryToDoRepository.kt
+++ src/main/kotlin/com/mergenc/repository/InMemoryToDoRepository.kt
@@ -1,13 +1,10 @@
 package com.mergenc.repository
 
 import com.mergenc.entities.ToDo
+import com.mergenc.entities.ToDoDraft
 
 class InMemoryToDoRepository : ToDoRepository {
-    private val todos = listOf<ToDo>(
-        ToDo(1, "Homework", true),
-        ToDo(2, "Internet Programlama", false),
-        ToDo(3, "Backend", true),
-    )
+    private val todos = mutableListOf<ToDo>()
 
     override fun getAllToDos(): List<ToDo> {
         return todos
@@ -15,5 +12,24 @@ class InMemoryToDoRepository : ToDoRepository {
 
     override fun getSingleToDo(id: Int): ToDo? {
         return todos.firstOrNull { it.id == id }
+    }
+
+    override fun addTodo(draft: ToDoDraft): ToDo {
+        val todo = ToDo(
+            id = todos.size + 1, title = draft.title, done = draft.done
+        )
+        todos.add(todo)
+        return todo
+    }
+
+    override fun removeTodo(id: Int): Boolean {
+        return todos.removeIf { it.id == id }
+    }
+
+    override fun updateTodo(id: Int, draft: ToDoDraft): Boolean {
+        val todo = todos.firstOrNull { it.id == id } ?: return false
+        todo.title = draft.title
+        todo.done = draft.done
+        return true
     }
 }
